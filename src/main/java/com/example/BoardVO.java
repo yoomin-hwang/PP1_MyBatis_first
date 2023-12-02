7362935fd53c440440b7e9a9f09015c5b1703a4e
hwangyoomin <yoominh3@gmail.com>
2023-12-02 12:13:06 +0900

    implemented BoardVO

--- src/main/java/com/example/BoardVO.java
+++ src/main/java/com/example/BoardVO.java
@@ -1,73 +1,49 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Date;
 
-import java.util.List;
+public class BoardVO {
+    private int seq;
+    private String title;
+    private String writer;
+    private String content;
+    private Date regdate;
+    private int cnt;
 
-
-@Controller
-@RequestMapping(value = "/board")
-public class BoardController {
-
-    @Autowired
-    BoardServiceImpl boardService;
-
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String boardlist(Model model) {
-        model.addAttribute("list", boardService.getBoardList());
-        return "list";
+    public int getSeq() {
+        return seq;
     }
-
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String addPost() {
-        return "addform";
+    public void setSeq(int seq) {
+        this.seq = seq;
     }
-
-    @RequestMapping(value = "/addok", method = RequestMethod.POST)
-    public String addPostOk(BoardVO vo) {
-        if (boardService.insertBoard(vo) == 0)
-            System.out.println("데이터 추가 실패 ");
-        else
-            System.out.println("데이터 추가 성공!!!");
-        return "redirect:list";
+    public String getTitle() {
+        return title;
     }
-
-    @RequestMapping(value = "/editform/{id}", method = RequestMethod.GET)
-    public String editPost(@PathVariable("id") int id, Model model) {
-        BoardVO boardVO = boardService.getBoard(id);
-        model.addAttribute("u", boardVO);
-        return "editform";
+    public void setTitle(String title) {
+        this.title = title;
     }
-
-    @RequestMapping(value = "/editok", method = RequestMethod.POST)
-    public String editPostOk(BoardVO vo) {
-        if(boardService.updateBoard(vo) == 0)
-            System.out.println("데이터 수정 실패 ");
-        else
-            System.out.println("데이터 수정 성공!!!");
-        return "redirect:list";
+    public String getWriter() {
+        return writer;
     }
-
-    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
-    public String viewPost(@PathVariable("id") int id, Model model) {
-        BoardVO boardVO = boardService.getBoard(id);
-        model.addAttribute("u", boardVO);
-        return "view";
+    public void setWriter(String writer) {
+        this.writer = writer;
     }
-
-
-    @RequestMapping(value = "/deleteok/{id}", method = RequestMethod.GET)
-    public String deletePostOk(@PathVariable("id") int id) {
-        if (boardService.deleteBoard(id) == 0)
-            System.out.println("데이터 삭제 실패 ");
-        else
-            System.out.println("데이터 삭제 성공!!!");
-        return "redirect:../list";
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public Date getRegdate() {
+        return regdate;
+    }
+    public void setRegdate(Date regdate) {
+        this.regdate = regdate;
+    }
+    public int getCnt() {
+        return cnt;
+    }
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
     }
 }
