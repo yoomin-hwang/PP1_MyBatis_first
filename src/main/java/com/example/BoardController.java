9a50a417644877b1a01003f7267ec13aa75b9721
hwangyoomin <yoominh3@gmail.com>
2023-12-03 16:48:09 +0900

    implemented view.jsp

--- src/main/java/com/example/BoardController.java
+++ src/main/java/com/example/BoardController.java
@@ -63,4 +63,11 @@ public class BoardController {
             System.out.println("데이터 삭제 성공!!!");
         return "redirect:list";
     }
+
+    @RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+    public String viewPost(@PathVariable("id") int id, Model model) {
+        BoardVO boardVO = boardService.getBoard(id);
+        model.addAttribute("u", boardVO);
+        return "view";
+    }
 }
--- src/main/webapp/WEB-INF/views/view.jsp
+++ src/main/webapp/WEB-INF/views/view.jsp
@@ -9,6 +9,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" isELIgnored="false"
          pageEncoding="UTF-8" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 
 <html>
 <head>
