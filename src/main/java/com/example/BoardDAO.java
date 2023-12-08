b8cee646471bccd0fe19644615c445c50551517b
hwangyoomin <yoominh3@gmail.com>
2023-12-08 10:08:53 +0900

    my batis created

--- src/main/java/com/example/BoardDAO.java
+++ src/main/java/com/example/BoardDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -15,52 +16,44 @@ import org.springframework.stereotype.Repository;
 public class BoardDAO {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
-
-    Connection conn = null;
-    PreparedStatement stmt = null;
-    ResultSet rs = null;
-
+    SqlSession sqlSession;
 
     public int insertBoard(BoardVO vo) {
-        String sql = "insert into BOARD (title, writer, content) values ("
-                + "'" + vo.getTitle() + "',"
-                + "'" + vo.getWriter() + "',"
-                + "'" + vo.getContent() + "')";
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.insert("Board.insertBoard", vo);
+        return result;
+    }
+
+    public BoardVO getBoard(int seq) {
+        BoardVO one = sqlSession.selectOne("Board.getBoard", seq);
+        return one;
+    }
+
+    public List<BoardVO> getBoardList() {
+        List<BoardVO> list = sqlSession.selectList("Board.getBoardList");
+        return list;
     }
 
     public int deleteBoard(int seq){
-        String sql = "delete from BOARD where seq =" + seq;
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.delete("Board.deleteBoard", seq);
+        return result;
     }
 
     public int updateBoard(BoardVO vo){
-        String sql = "update BOARD set "
-                +"title='"+vo.getTitle()+"',"
-                +"writer='"+vo.getWriter()+"',"
-                +"content='"+vo.getContent()+"' where seq=" +vo.getSeq();
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.update("Board.updateBoard", vo);
+        return result;
     }
 
-    static class BoardRowMapper implements RowMapper<BoardVO> {
-        @Override
-        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
-            BoardVO vo = new BoardVO();
-            vo.setSeq(rs.getInt("seq"));
-            vo.setTitle(rs.getString("title"));
-            vo.setContent(rs.getString("content"));
-            vo.setWriter(rs.getString("writer"));
-            vo.setRegdate(rs.getDate("regdate"));
-            return vo;
-        }
-    }
-    public BoardVO getBoard(int seq) {
-        String sql = "select * from BOARD where seq=" + seq;
-        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
-    }
-    public List<BoardVO> getBoardList() {
-        String sql = "select * from BOARD order by regdate desc";
-        return jdbcTemplate.query(sql, new BoardRowMapper());
-    }
+//    static class BoardRowMapper implements RowMapper<BoardVO> {
+//        @Override
+//        public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//            BoardVO vo = new BoardVO();
+//            vo.setSeq(rs.getInt("seq"));
+//            vo.setTitle(rs.getString("title"));
+//            vo.setContent(rs.getString("content"));
+//            vo.setWriter(rs.getString("writer"));
+//            vo.setRegdate(rs.getDate("regdate"));
+//            return vo;
+//        }
+//    }
+
 }
